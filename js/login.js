f4e6bea4fc60e7b20c5342501d513b0163e24dd8
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2023-06-28 13:45:47 -0500

    Login completo

---
+++ js/login.js
@@ -0,0 +1,20 @@
+const form = document.querySelector(".input-name");
+const inputName = document.querySelector("input[type=text]");
+const buttonLogin = document.querySelector(".login-button");
+const buttonLoginSvg = document.querySelector(".login-button svg");
+
+inputName.addEventListener('input', ({ target }) => {
+    if(target.value.length < 2) {
+        buttonLogin.classList.add("disabled");
+        return;
+    }
+
+    buttonLogin.classList.remove("disabled");
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    localStorage.setItem("player", inputName.value);
+    window.location = "./pages/quizes.html";
+});
