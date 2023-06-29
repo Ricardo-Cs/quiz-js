961f90059abff774bfa655fdbff5a56952609dec
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2023-06-28 21:46:31 -0500

    Inicia parte de imprimir questão na tela

--- js/questoes.js
+++ js/questoes.js
@@ -1,14 +1,60 @@
 export const quizData = {
     programming: [
         {
-            question: "Questão número 1",
-            options: ["Resposta 1", "Resposta 2", "Resposta 3", "Resposta 4"],
+            question: "Questão de programação número 1",
+            options: ["Resposta a1", "Resposta a2", "Resposta a3", "Resposta a4"],
             answer: 0            
         },
         {
-            question: "Questão número 2",
+            question: "Questão de programação número 2",
+            options: ["Resposta b2", "Resposta b2", "Resposta b3", "Resposta b4"],
+            answer: 1
+        },
+        {
+            question: "Questão de programação número 3",
+            options: ["Resposta c1", "Resposta c2", "Resposta c3", "Resposta c4"],
+            answer: 1
+        },
+        {
+            question: "Questão de programação número 4",
+            options: ["Resposta d1", "Resposta d2", "Resposta d3", "Resposta d4"],
+            answer: 1
+        }
+    ],
+    conhecimentos: [
+        {
+            question: "Questão de conhecimentos gerais número 1",
             options: ["Resposta 1", "Resposta 2", "Resposta 3", "Resposta 4"],
             answer: 1
+        },
+        {
+            question: "Questão de conhecimentos gerais número 2",
+            options: ["Resposta 1", "Resposta 2", "Resposta 3", "Resposta 4"],
+            answer: 3
+        }
+    ],
+    games: [
+        {
+            question: "Questão de jogos número 1",
+            options: ["Resposta 1", "Resposta 2", "Resposta 3", "Resposta 4"],
+            answer: 1
+        },
+        {
+            question: "Questão de jogos número 2",
+            options: ["Resposta 1", "Resposta 2", "Resposta 3", "Resposta 4"],
+            answer: 2
+        }
+    ],
+    science: [
+        {
+            question: "Questão de ciências número 1",
+            options: ["Resposta 1", "Resposta 2", "Resposta 3", "Resposta 4"],
+            answer: 0
+        },
+        {
+            question: "Questão de ciências número 2",
+            options: ["Resposta 1", "Resposta 2", "Resposta 3", "Resposta 4"],
+            answer: 3
         }
     ]
 }
--- js/quiz.js
+++ js/quiz.js
@@ -1,9 +1,58 @@
 import { quizData } from "./questoes.js";
 
 const quizQuestion = document.querySelector(".quiz-question h2");
+const quizOptions = document.querySelectorAll(".quiz-options span");
 const urlParams = new URLSearchParams(window.location.search);
 const quizParams = urlParams.get("quiz");
 
-// if(quizParams === "1") {
-//     quizQuestion.innerHTML = quizData.programming[0].question;
-// }
+const nextQuestion = () => {
+    currentQuestion++;
+    window.localStorage.setItem("currentQuestion", currentQuestion);
+    loadQuestion();
+}
+
+const loadOptions = (quizOptions, quizData, type) => {
+    quizOptions.forEach((option, index) => {
+      option.innerHTML = quizData[type][currentQuestion].options[index];
+    });
+  };
+  
+
+let currentQuestion = 0;
+let setLocalStorage = window.localStorage.getItem("currentQuestion");
+
+function loadQuestion() {
+    if (quizParams === "1") {
+        quizQuestion.innerHTML = quizData.programming[currentQuestion].question;
+        loadOptions(quizOptions, quizData, "programming");
+        return;
+    }
+      
+
+    if (quizParams === "2") {
+        quizQuestion.innerHTML = quizData.conhecimentos[currentQuestion].question;
+        loadOptions(quizOptions, quizData, "conhecimentos");
+        return;
+    } 
+
+    if (quizParams === "3") {
+        quizQuestion.innerHTML = quizData.games[currentQuestion].question;
+        loadOptions(quizOptions, quizData, "games");
+        return;
+    } 
+
+    if (quizParams === "4") {
+        quizQuestion.innerHTML = quizData.science[currentQuestion].question;
+        loadOptions(quizOptions, quizData, "science");
+        return;
+    } 
+
+}
+
+loadQuestion();
+
+quizOptions[0].addEventListener('click', nextQuestion);
+quizOptions[1].addEventListener('click', nextQuestion);
+quizOptions[2].addEventListener('click', nextQuestion);
+quizOptions[3].addEventListener('click', nextQuestion);
+
