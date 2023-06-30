ecee87d0d23b99f182a2f69c26fc770987e9b176
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2023-06-30 15:23:20 -0500

    Cria respostas de programação e estiliza quiz

--- js/questoes.js
+++ js/questoes.js
@@ -1,25 +1,71 @@
 export const quizData = {
     programming: [
         {
-            question: "Questão de programação número 1",
-            options: ["Resposta a1", "Resposta a2", "Resposta a3", "Resposta a4"],
+            question: "Quais os nomes das funções que convertem um objeto literal em um Json, e um Json em objeto literal, respectivamente.",
+            options: ["JSON.stringfy() e JSON.parse()", "JSON.toJson() e JSON.toObject()", "JSON.convertJson() e JSON.convertObject()", "JSON.parse() e JSON.toObject()"],
             answer: 0            
         },
         {
-            question: "Questão de programação número 2",
-            options: ["Resposta b2", "Resposta b2", "Resposta b3", "Resposta b4"],
+            question: "Segundo a documentação oficial do React.js, o que é o React?",
+            options: ["Framework", "Biblioteca", "Gerenciador de interfaces", "Gerenciador de dependências"],
             answer: 1
         },
         {
-            question: "Questão de programação número 3",
-            options: ["Resposta c1", "Resposta c2", "Resposta c3", "Resposta c4"],
+            question: "Em PHP, como se é declarado uma variável?",
+            options: ["var = 1", "$var = 1", "var variavel = 1", "#var = 1"],
             answer: 1
         },
         {
-            question: "Questão de programação número 4",
-            options: ["Resposta d1", "Resposta d2", "Resposta d3", "Resposta d4"],
+            question: "O que significa a sigla HTML?",
+            options: ["Home Tool Markup Language", "Hyperlink Markup Language", "Hyper Textual Markup Language", "Hypertext Markup Language"],
+            answer: 3
+        },
+        {
+            question: "O que é uma função recursiva em programação?",
+            options: ["Uma função recursiva é uma função que não permite a passagem de argumentos.",
+                "Uma função recursiva é uma função que só pode ser chamada uma vez dentro de um programa.",
+                "Uma função recursiva é uma função que chama a si mesma dentro do seu próprio corpo.",
+                "Hypertext Markup Language"],
+            answer: 2
+        },
+        {
+            question: "O que é uma chave primária em um banco de dados relacional?",
+            options: [" A chave primária é um índice secundário que permite acesso rápido aos dados em uma tabela.",
+                "A chave primária é um tipo de restrição que impede a inserção de valores duplicados em uma tabela.",
+                "A chave primária é um campo em uma tabela que garante a unicidade e a identificação única de cada registro.",
+                "A chave primária é um campo de uma tabela que armazena dados relacionados a chaves estrangeiras em outras tabelas."],
+            answer: 2
+        },
+        {
+            question: "O que é o operador ternário em programação?",
+            options: ["O operador ternário é um operador lógico que realiza operações booleanas entre dois valores.",
+                "O operador ternário é um operador matemático que realiza cálculos com três operandos.",
+                "O operador ternário é um operador condicional que avalia uma expressão booleana e retorna um valor com base nessa avaliação.",
+                "O operador ternário é um recurso para tomada de decisões com objetivo similar ao do if/else, mas que é codificado em apenas uma linha."],
+            answer: 3
+        },
+        {
+            question: "Qual foi o primeiro computador criado?",
+            options: ["O primeiro computador foi o ENIAC (Electronic Numerical Integrator and Computer), desenvolvido nos Estados Unidos.",
+                "O primeiro computador foi o Z1, desenvolvido pelo engenheiro alemão Konrad Zuse.",
+                "O primeiro computador foi o Colossus, criado pelos britânicos durante a Segunda Guerra Mundial para decifrar códigos nazistas.",
+                "O primeiro computador foi o Harvard Mark I, construído em Harvard nos Estados Unidos."],
+            answer: 0
+        },
+        {
+            question: "Qual a diferença entre os operadores de comparação de valores '==' e '===' em Javascript?",
+            options: ["Ambos os operadores '==' e '===' comparam os valores e os tipos de dados das variáveis.",
+                "O operador '==' compara apenas os valores das variáveis, ignorando seus tipos de dados.",
+                "O operador '===' compara apenas os valores das variáveis, ignorando seus tipos de dados.",
+                "O operador '==' compara apenas os valores das variáveis, já o operador '===' compara os valores e os tipos das variáveis."],
+            answer: "3"
+        },
+        {
+            question: "O que significa a sigla JSON?",
+            options: ["JavaScript Object Notation.", "Java Syntax Object Notation.", "JavaScript Oriented Notation.", "Java Object Naming."],
             answer: 1
         }
+
     ],
     conhecimentos: [
         {
--- js/quiz.js
+++ js/quiz.js
@@ -4,55 +4,57 @@ const quizQuestion = document.querySelector(".quiz-question h2");
 const quizOptions = document.querySelectorAll(".quiz-options span");
 const urlParams = new URLSearchParams(window.location.search);
 const quizParams = urlParams.get("quiz");
+let currentQuestion = 0;
+// currentQuestion = window.localStorage.getItem("currentQuestion");
 
 const nextQuestion = () => {
-    currentQuestion++;
-    window.localStorage.setItem("currentQuestion", currentQuestion);
-    loadQuestion();
+    if(loadQuestion()) {
+        currentQuestion++;
+        window.localStorage.setItem("currentQuestion", currentQuestion);
+        loadQuestion();
+    }
 }
+
 
 const loadOptions = (quizOptions, quizData, type) => {
     quizOptions.forEach((option, index) => {
       option.innerHTML = quizData[type][currentQuestion].options[index];
     });
-  };
+};
   
-
-let currentQuestion = 0;
-let setLocalStorage = window.localStorage.getItem("currentQuestion");
+for(let i = 0; i < quizOptions.length; i++) {
+    quizOptions[i].addEventListener('click', nextQuestion);
+}
 
 function loadQuestion() {
     if (quizParams === "1") {
         quizQuestion.innerHTML = quizData.programming[currentQuestion].question;
         loadOptions(quizOptions, quizData, "programming");
-        return;
+        return true;
     }
       
 
     if (quizParams === "2") {
         quizQuestion.innerHTML = quizData.conhecimentos[currentQuestion].question;
         loadOptions(quizOptions, quizData, "conhecimentos");
-        return;
+        return true;
     } 
 
     if (quizParams === "3") {
         quizQuestion.innerHTML = quizData.games[currentQuestion].question;
         loadOptions(quizOptions, quizData, "games");
-        return;
+        return true;
     } 
 
     if (quizParams === "4") {
         quizQuestion.innerHTML = quizData.science[currentQuestion].question;
         loadOptions(quizOptions, quizData, "science");
-        return;
+        return true;
     } 
 
-}
+    return false;
 
+}
 loadQuestion();
 
-quizOptions[0].addEventListener('click', nextQuestion);
-quizOptions[1].addEventListener('click', nextQuestion);
-quizOptions[2].addEventListener('click', nextQuestion);
-quizOptions[3].addEventListener('click', nextQuestion);
 
